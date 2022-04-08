cafa58504459983e6a890f5176b94b8da99c7626
Tejash Panda <52340554+Tpanda03@users.noreply.github.com>
2022-04-07 20:44:43 -0500

    Update main.cpp

--- Hoverpod/IMU/main.cpp
+++ Hoverpod/IMU/main.cpp
@@ -1,11 +1,25 @@
 //#include "IMU.hpp"
-//#include "Kinematics.hpp"
+#include "Kinematics.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
+struct acceleration { //three acceleration vectors
+  double Ax,Ay,Az = 0;
+};
+struct velocity { //three velocity vectors
+  double Vx,Vy,Vz = 0;
+};
+struct position { //three position vectors
+  double Px,Py,Pz = 0;
+};
+
+
 int main() {
+  Kinematics temp;
   //parse measures.txt
   string line;
   ifstream readFile ("measures.txt");
@@ -17,7 +31,15 @@ int main() {
     while ( getline (readFile,line) )
     {
       if (count == 4) {
-        writeFile << line << "at time t = " << time << '\n';
+        for (int i = 0; i < line.length(); i++) {
+          if (isdigit(line[i]) || line[i] == '-' || line[i] == '.') {
+            line[i] = line[i];
+          }
+          else {
+            line[i] = ' ';
+          }
+        }
+        writeFile << line << " " << time << '\n';
         count = 0;
         time++;
       }
@@ -30,8 +52,25 @@ int main() {
   else cout << "Unable to open file";
 
   //from current and previous acceleration, find current and previous velocity and current position
+  ifstream accelFile ("accel.txt");
+  if (accelFile.is_open()) {
+    while (getline(accelFile, line)) {
+      cout << line << endl;
+      istringstream iss (line);
+      //get Ax, Ay, Az from t0 and Ax, Ay, Az from t1
+      //Ax Ay Az t
+      //from t0 and t1 find velocity at t1
+      //update to velocity text file as Vx Vy Vz td
+
+    }
+  }
+
+  //Finding current and previous velocities and using it to find position at time t
 
   //create a running total sum of averages throughout runtime
+
+
+  //my tests
 
   /*
   essentially, the kinematics class focuses on getting acceleration data, and finding velocity and position
